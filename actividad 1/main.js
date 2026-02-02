00b9c4f2b4a5a2f5ee3af02350e9755c7cc5785e
francolobo1709 <lobofranconicolas@gmail.com>
2026-02-02 09:21:00 -0300

    feat: inicialización del proyecto con actividades 1, 2 y 3

---
+++ actividad 1/main.js
@@ -0,0 +1,14 @@
+
+let nombre = "franco";
+let edad = 38;
+let mensaje = `Hola, mi nombre es ${nombre} y tengo ${edad} años.`;
+let altura = 1.73;
+let estudiante = true;
+let trabaja = true;
+
+console.log("--- Valores de las variables ---");
+console.log(nombre);
+console.log(edad);
+console.log(estudiante);
+console.log(trabaja);
+console.log(altura);
